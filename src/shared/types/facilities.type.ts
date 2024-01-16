0d4e9fd16a6f3f1fa9a759ad83bab1184f6bd513
Vladimir Rybas <wlrybas@gmail.com>
2024-01-16 18:07:34 +0300

    Добавлен функционал для чтения файла TSV

--- src/shared/types/facilities.type.ts
+++ src/shared/types/facilities.type.ts
@@ -1,10 +1,21 @@
 export type Facilities = {
-    Breakfast: 'Breakfast';
-    Conditioning: 'Air Conditioning';
-    Laptop: 'Laptop';
-    Workspace: 'Friendly workspace';
-    Babyseat: 'Baby seat';
-    Washer: 'Washer';
-    Towels: 'Towels';
-    Fridge: 'Fridge'
+  Breakfast: 'Breakfast';
+  Conditioning: 'Air Conditioning';
+  Laptop: 'Laptop';
+  Workspace: 'Friendly workspace';
+  Babyseat: 'Baby seat';
+  Washer: 'Washer';
+  Towels: 'Towels';
+  Fridge: 'Fridge';
 };
+
+// export enum Facilities {
+//   Breakfast = 'Breakfast',
+//   Conditioning = 'Air Conditioning',
+//   Laptop = 'Laptop',
+//   Workspace = 'Friendly workspace',
+//   Babyseat = 'Baby seat',
+//   Washer = 'Washer',
+//   Towels = 'Towels',
+//   Fridge = 'Fridge',
+// }
