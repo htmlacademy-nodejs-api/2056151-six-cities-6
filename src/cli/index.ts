933e516688de9c65e736c4b8416c677f704ee58e
Vladimir Rybas <wlrybas@gmail.com>
2024-01-29 15:59:13 +0300

    Создана команда для формирования объявлений

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,3 +3,4 @@ export * from './command-parser.js';
 export * from './commands/help.command.js';
 export * from './commands/version.commands.js';
 export * from './commands/import.commands.js';
+export * from './commands/generate.command.js';
