0eca7588649c70d56140433d7c2c433ca8438371
Vladimir Rybas <wlrybas@gmail.com>
2024-01-29 17:39:35 +0300

    Добавлен генератор в команду <code>Generate command</code>

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,13 +1,14 @@
 import got from 'got';
+import { appendFile } from 'node:fs/promises';
 
 import { Command } from './commands.interface.js';
 import { MockServerData } from '../../shared/types/index.js';
-
+import { TSVOfferGenerator } from '../../shared/libs/offer-generator/index.js';
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
 
-  private async load (url: string) {
+  private async load(url: string) {
     try {
       this.initialData = await got.get(url).json();
     } catch {
@@ -15,16 +16,28 @@ export class GenerateCommand implements Command {
     }
   }
 
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
+        encoding: 'utf8',
+        e,
+      });
+    }
+  }
+
   public getName(): string {
     return '--generate';
   }
 
-  public async execute(...parameters: string[]):Promise<void> {
+  public async execute(...parameters: string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
 
     try {
       await this.load(url);
+      await this.write(filepath, offerCount);
+      console.info(`File ${filepath} was created!`)
     } catch (error: unknown) {
       console.error('Can\'t generate data');
 
