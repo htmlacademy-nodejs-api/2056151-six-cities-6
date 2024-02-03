b4e6b8f18e9cf0075b5bdb1e3e93ad533889e87f
Vladimir Rybas <wlrybas@gmail.com>
2024-02-03 15:07:05 +0300

    Обновлен класс `RestConfig`

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,7 +1,11 @@
 import { Logger } from '../shared/libs/logger/index.js';
+import { Config, RestSchema } from '../shared/libs/сonfig/index.js';
 
 export class RestApplication {
-  constructor(private readonly logger: Logger) {}
+  constructor(
+    private readonly logger: Logger,
+    private readonly config: Config<RestSchema>
+  ) {}
 
   public async init() {
     this.logger.info('Application initialization');
