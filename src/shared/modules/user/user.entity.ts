d2bd886cc60628c77773abbba0d10575a79bb8b3
Vladimir Rybas <wlrybas@gmail.com>
2024-02-08 17:21:13 +0300

    Добавлено определение модели и схемы

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,19 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true })
   public username: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: true })
   public password: string;
-  public avatarUrl?: string;
+
+  @prop({ required: false, default: '' })
+  public avatarUrl: string;
+
+  @prop({ required: true })
   public isPro: boolean;
 }
